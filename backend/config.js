8fd5009d3dfb94836374383b797d8b45a646fbfd
Boosty-Labs-hiring <ezraloomis.gears@gmail.com>
2026-06-18 17:35:24 +0900

    ADMIN-57289 : Extracted module for interview

---
+++ backend/config.js
@@ -0,0 +1,12 @@
+module.exports = { 
+  mysqlHost: "13.233.12.75",
+    user: "",
+    password: '',
+    database: "",
+    mysqlPort: 3306,
+    JWT_SECRET_KEY: '',
+    SESSION_EXPIRES_IN: '24h',
+    imageUrl:'',
+    contractAddress : '',
+    clientDepositAddress:'',
+}
